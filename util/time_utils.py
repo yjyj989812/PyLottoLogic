d9543f3376e8d6e08d7977df298bf17189a95293
JeongYeon Yoo <yjyj989812@naver.com>
2024-10-16 20:31:15 +0900

    [INITAL] init new file: this file support time functions

---
+++ util/time_utils.py
@@ -0,0 +1,20 @@
+import datetime
+
+def next_saturday_evening():
+    now = datetime.datetime.now()
+    
+    # 현재 요일 (월=0, 화=1, ..., 토=5, 일=6)
+    today_weekday = now.weekday()
+    
+    # 이번 주 토요일 날짜 계산 (토요일은 5)
+    days_until_saturday = (5 - today_weekday) if today_weekday <= 5 else (5 - today_weekday + 7)
+    next_saturday = now + datetime.timedelta(days=days_until_saturday)
+    
+    # 토요일 20시 40분으로 설정
+    next_saturday_evening = next_saturday.replace(hour=20, minute=35, second=0, microsecond=0)
+    
+    # 현재 시간이 토요일 20시 35분 이후인 경우, 다음 주 토요일로 이동
+    if now > next_saturday_evening:
+        next_saturday_evening += datetime.timedelta(days=7)
+    
+    return next_saturday_evening
--- util/util_init.md
+++
@@ -1,9 +0,0 @@
-# util 기능 목록
-- 2024-09-16
-## List
-### get_data.py
-- ./data에서 각 번호의 뽑힌 횟수와 진행 횟수 반환
-### get_timestamp.py
-- 가장 가까운 복권 추첨일과 시간 반환
-### export_numbers.py
-- 예측한 번호를 다른 매체에게 전달
