0de56032bd3d873f7b2e26a21b9b60fd27d1b81b
JeongYeon Yoo <yjyj989812@naver.com>
2024-10-17 13:22:34 +0900

    [FIX] fix dump_numbers_to_json

--- src/data_utils.py
+++ src/data_utils.py
@@ -6,11 +6,11 @@ def crawl_data(parsing_url):
 def get_numbers_in_request(request_result, pattern=r"drwtNoPop\[\d+\] = \'(\d+)\'"):
     return re.findall(pattern, request_result.text)
 
-def dump_numbers_to_json(data, file_path="./data/number_count.json"):
+def dump_numbers_to_json(data, file_path="./data/number_count.json", update_date="2024-10-17"):
     json_format = {
         "version": "2024-10-16",
         "statement": "Save count of lotto numbers",
-        "last_update": "2024-10-17",
+        "last_update": update_date,
         "data": {}
     }
 
