1eb04313cffcde442d7ecada9c858e5b56916ecb
JeongYeon Yoo <yjyj989812@naver.com>
2024-12-21 11:14:22 +0900

    [UPDATE] suggest numbers are sorted

--- src/random_utils.py
+++ src/random_utils.py
@@ -6,4 +6,4 @@ def choice_numbers(_data, _sid_time, _size=6):
     
     np.random.seed(int(_sid_time.timestamp()))
     selected_numbers = np.random.choice(numbers, size=_size, replace=False, p=np.array(weights) / sum(weights))
-    return map(int, selected_numbers)
+    return sorted(list(map(int, selected_numbers)))
